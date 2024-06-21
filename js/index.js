90917af2b4f5923675c5a6c67721f58d7c00add7
Miha77777 <lv11miha@gmail.com>
2024-06-21 21:38:40 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// 1
+const myAge = 13;
+console.log(myAge);
+
+// 2
+const myName = "Mykhailo";
+console.log(myName);
+
+// 3
+const isStudent = true;
+console.log(isStudent);
+
+// 4
+const myString = "Чужому научайся й свого не цурайся";
+console.log(myString);
+
+// 5
+let myNumber = 10;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+// 6
+const myNull = null;
+console.log(myNull);
+
+// 7
+const answerPrompt = prompt("Your name: ");
+console.log(answerPrompt);
+
+// 8
+const answerConfirm = confirm("Підтвердіть дію");
+console.log(answerConfirm ? "Дякую за підтвердження!" : "Дію відмінено!");
+
+// 9
+alert("Ця дія небезпечна!")
+const answerDangerousConfirm = confirm("Підтвердіть дію");
+console.log(answerDangerousConfirm ? "Дякую за підтвердження!" : "Дію відмінено!");
